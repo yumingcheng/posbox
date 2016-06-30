a5d92e813030ad971078dfc1e724e1cc1f8c7f76
maoye <598086951@qq.com>
2016-06-30 16:44:23 +0800

    修改一些bug

--- static/sell-base.js
+++ static/sell-base.js
@@ -250,14 +250,14 @@ function searchCustomerInfo(){
 					$("#x_customer_number").text(obj.cardnumber)	
 					$("#x_customer_number").attr("success","true")				
 					$("#x_customer_name").text(obj.name == "" ? "空" : obj.name)
-					$("#x_customer_points").text(obj.points == "" ? "空" : obj.points)
+					$("#x_customer_points").text(obj.points == "" ? "0" : obj.points)
 					$("#x_customer_phone").text(obj.phonenumber == "" ? "空" : obj.phonenumber)
 					$(".input-lg p").css("color","")
 				}else{
 					$("#x_customer_number").attr("success","flase")				
 					$("#x_customer_number").text("卡号\"" + obj.cardnumber + "\"不存在!")
 					$("#x_customer_name").text("空")
-					$("#x_customer_points").text("空")
+					$("#x_customer_points").text("0")
 					$("#x_customer_phone").text("空")
 					$(".input-lg p").css("color","red")
 				}
@@ -390,7 +390,7 @@ function discountFull() {
 }
 
 function sellForCustomerEx() {
-	if($("#x_customer_number").attr("success") == "true"){
+	if($("#x_customer_number").attr("success") == "true" && $("#ac_customer_no").val() != ""){
 		$("#r_customer_info").val($("#x_customer_number").text() + "/" + $("#x_customer_name").text() + "/" + $("#x_customer_points").text())
 	}else{
 		$("#r_customer_info").val("")
@@ -414,6 +414,7 @@ function clearCustomer(){
 
 
 $(document).ready(function() {
+    var timeout;
 	$("#bar_code").focus()
 	toastr.options = {
   		"closeButton": false,
@@ -503,7 +504,15 @@ $(document).ready(function() {
         	sellForCustomerEx();
         	$("#bar_code").focus()
         }else{
-        	"" != $("#ac_customer_no").val() && searchCustomerInfo()
+            if("" != $("#ac_customer_no").val()){
+                clearTimeout(timeout);
+                timeout=setTimeout(function(){ // setting the delay for each keypress
+                    searchCustomerInfo()
+                }, 100);
+            }
+            
+
+        	// "" != $("#ac_customer_no").val() && searchCustomerInfo()
         }
 
     }),
